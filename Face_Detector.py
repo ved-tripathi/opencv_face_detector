8255ae08f16c4d0543470ef702bb66533f842c2e
ved <67370285+ved-tripathi@users.noreply.github.com>
2021-11-11 19:39:05 +0530

    Add files via upload

---
+++ Face_Detector.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+from random import randrange
+
+#training the machine 
+trained_face_data = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#calling the test photo
+# img = cv.imread('heavy_test.jpg')
+webcam = cv.VideoCapture(0)
+
+
+w
+
+    ### to stop, press Q
+    if key == 81 or key == 113 :
+        break
+
+# release
+webcam.release()
+
+#
+# #holding the output
+# cv.waitKey()
+print("have a good day")
